4aac1afc1abb05216c8ec7b90ec87acb460d9ac0
Ivan Jovanovic <ivojlo2002@gmail.com>
2025-02-07 17:36:00 +0100

    removed imports

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+
 import { environment } from '../../environments/environment.development';
 import { Project } from '../models/project.model';
 import { ExtendedProject } from '../models/extended-project.model';
